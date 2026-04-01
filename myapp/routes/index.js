a8d1bebbdfd17e8234484570fa408292f91564b6
Mathis Millot <mathismillot05@gmail.com>
2026-04-01 11:39:44 +0200

    install module + archi

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* Page d'accueil */
+router.get('/', function(req, res) {
+  res.render('html/accueil');
+});
+
+router.get('/accueil', function(req, res) {
+  res.render('html/accueil');
+});
+
+/* Offres d'emploi */
+router.get('/offres', function(req, res) {
+  res.render('html/offres');
+});
+
+/* Connexion / Inscription */
+router.get('/connection', function(req, res) {
+  res.render('html/connection');
+});
+
+router.get('/inscription_candidat', function(req, res) {
+  res.render('html/inscription_candidat');
+});
+
+router.get('/inscription_recruteur', function(req, res) {
+  res.render('html/inscription_recruteur');
+});
+
+router.get('/responsable_recrutement', function(req, res) {
+  res.render('html/responsable_recrutement');
+});
+
+/* Espace candidat */
+router.get('/candidature', function(req, res) {
+  res.render('html/candidature');
+});
+
+router.get('/profil_professionnel', function(req, res) {
+  res.render('html/profil_professionnel');
+});
+
+router.get('/informations_personnelles', function(req, res) {
+  res.render('html/informations_personnelles');
 });
 
 module.exports = router;
